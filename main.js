9db0122ebae78261095f94c41c5826e4ab157aa8
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2024-02-26 14:36:45 +0100

    finishing touches html&css validated

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const localStorageTheme = localStorage.getItem("theme");
-
 const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 const initialTheme = calculateSettingAsThemeString({
   localStorageTheme,
@@ -16,23 +15,13 @@ const sunSvgPath = "/img/sun.svg";
 const moonSvgPath = "/img/moon.svg";
 
 buttons.forEach((button) => {
-  // Set initial icon based on stored theme for each button
   const initialIconSrc = initialTheme === "dark" ? moonSvgPath : sunSvgPath;
   button.querySelector("img").src = initialIconSrc;
-
   button.addEventListener("click", () => {
     const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-
-    // update theme attribute on HTML to switch theme in CSS
     document.querySelector("html").setAttribute("data-theme", newTheme);
-
-    // update in local storage
     localStorage.setItem("theme", newTheme);
-
-    // update the currentThemeSetting in memory
     currentThemeSetting = newTheme;
-
-    // Update the src attribute of the img based on the theme for each button
     const newSrc = newTheme === "dark" ? moonSvgPath : sunSvgPath;
     button.querySelector("img").src = newSrc;
   });
@@ -45,11 +34,9 @@ function calculateSettingAsThemeString({
   if (localStorageTheme !== null) {
     return localStorageTheme;
   }
-
   if (systemSettingDark.matches) {
     return "dark";
   }
-
   return "light";
 }
 function toggleDropdown() {
@@ -68,11 +55,9 @@ window.addEventListener("click", function (event) {
   var catDrop = document.getElementById("categoryDropdown");
   var dropdownContentCat = document.getElementById("categoryDropdownContent");
   var hamburgerMenu = document.querySelector(".hamburger-menu");
-
   if (!userDrop.contains(event.target)) {
     dropdownContent.classList.remove("show");
   }
-
   if (!catDrop.contains(event.target)) {
     dropdownContentCat.classList.remove("showCat");
   }
@@ -93,10 +78,8 @@ function toggleMenu() {
 window.addEventListener("resize", function () {
   var screenWidth = window.innerWidth;
   var hamburgerMenu = document.querySelector(".hamburger-menu");
-
-  if (screenWidth >= 750) {
+  if (screenWidth >= 730) {
     hamburgerMenu.classList.remove("active");
-    console.log("removed .active cuz screen size >730px");
   }
 });
 
