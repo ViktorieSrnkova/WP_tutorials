a2f852121d86d37755b77f47569a01abe394d0e4
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2024-02-26 11:29:52 +0100

    delete flags, refactor js, responsivity fix

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-// get theme on page load
 const localStorageTheme = localStorage.getItem("theme");
 
-// set initial theme
 const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 const initialTheme = calculateSettingAsThemeString({
   localStorageTheme,
   systemSettingDark,
 });
 
-// set theme on button press
 let currentThemeSetting = initialTheme;
 
-// set initial theme attribute on HTML
 document.querySelector("html").setAttribute("data-theme", initialTheme);
 
 const buttons = document.querySelectorAll(".dark-mode-switch");
 
-// Reference to the SVG files
 const sunSvgPath = "/img/sun.svg";
 const moonSvgPath = "/img/moon.svg";
 
@@ -72,6 +67,7 @@ window.addEventListener("click", function (event) {
   var dropdownContent = document.getElementById("userDropdownContent");
   var catDrop = document.getElementById("categoryDropdown");
   var dropdownContentCat = document.getElementById("categoryDropdownContent");
+  var hamburgerMenu = document.querySelector(".hamburger-menu");
 
   if (!userDrop.contains(event.target)) {
     dropdownContent.classList.remove("show");
@@ -80,46 +76,13 @@ window.addEventListener("click", function (event) {
   if (!catDrop.contains(event.target)) {
     dropdownContentCat.classList.remove("showCat");
   }
+  if (
+    !event.target.classList.contains("hamburger-icon") &&
+    !hamburgerMenu.contains(event.target)
+  ) {
+    hamburgerMenu.classList.remove("active");
+  }
 });
-
-let lang = navigator.language.slice(0, 2); // Get the first two letters of user's language preference
-
-// Hide elements that don't correspond to the user's preferred language
-if (lang === "en") {
-  document
-    .querySelectorAll(".second-language")
-    .forEach((elem) => (elem.style.display = "none"));
-} else {
-  document
-    .querySelectorAll(".english")
-    .forEach((elem) => (elem.style.display = "none"));
-}
-
-function goToEnglish() {
-  let currentUrl = window.location.href;
-  let newUrl;
-
-  if (currentUrl.includes("/cz/")) {
-    newUrl = currentUrl.replace("/cz/", "/en/");
-    window.location.href = newUrl;
-  }
-}
-
-function goToCzech() {
-  let currentUrl = window.location.href;
-  let newUrl;
-
-  if (currentUrl.includes("/en/")) {
-    newUrl = currentUrl.replace("/en/", "/cz/");
-    window.location.href = newUrl;
-  }
-}
-document.getElementById("english-image").addEventListener("click", goToEnglish);
-document.getElementById("czech-image").addEventListener("click", goToCzech);
-document
-  .getElementById("english-image-b")
-  .addEventListener("click", goToEnglish);
-document.getElementById("czech-image-b").addEventListener("click", goToCzech);
 
 function toggleMenu() {
   var menu = document.querySelector(".hamburger-menu");
@@ -137,21 +100,10 @@ window.addEventListener("resize", function () {
   }
 });
 
-// Click listener for hamburger menu icon
 var hamburgerIcon = document.querySelector(".hamburger-icon");
 hamburgerIcon.addEventListener("click", function (event) {
   event.stopPropagation();
   toggleMenu();
-});
-
-document.addEventListener("click", function (event) {
-  var hamburgerMenu = document.querySelector(".hamburger-menu");
-  if (
-    !event.target.classList.contains("hamburger-icon") &&
-    !hamburgerMenu.contains(event.target)
-  ) {
-    hamburgerMenu.classList.remove("active");
-  }
 });
 
 window.addEventListener("load", function () {
